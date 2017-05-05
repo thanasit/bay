351c7a828dc4abce367895139f4a796d1deb8e61
Kritsana Piriyakitpaiboon <kritsanapiriyakitpai@nbnco.com.au>
2017-05-05 15:47:56 +0700

    My RHQ server and script

---
+++ scripts/jon-server/profile.d/jon-agent.sh
@@ -0,0 +1,6 @@
+export RHQ_JAVA_HOME="/usr/lib/jvm/jre"
+export RHQ_AGENT_USER="jon"
+export RHQ_AGENT_HOME="/apps/jon/rhq-agent"
+export RHQ_AGENT_START_COMMAND="su -m ${RHQ_AGENT_USER} -c '${RHQ_AGENT_HOME}/bin/rhq-agent.sh'"
+export RHQ_AGENT_ADDITIONAL_JAVA_OPTS="-Djava.util.prefs.userRoot=${RHQ_AGENT_HOME}"
+export RHQ_CONTROL_ADDITIONAL_JAVA_OPTS="-Djava.util.prefs.userRoot=${RHQ_AGENT_HOME}"
---
+++ scripts/jon-server/profile.d/jon-server.sh
@@ -0,0 +1,3 @@
+export RHQ_JAVA_HOME="/usr/lib/jvm/jre"
+export RHQ_SERVER_HOME="/apps/jon/jon-server-3.3.0.GA"
+export RHQ_SERVER_USER="jon"
