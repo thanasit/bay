ccdcd37c388179ba18172cbc53c237bc2bbbd22a
tthetprasit_gmail <cpukaitong15@gmail.com>
2018-01-25 11:54:29 +0700

    edit config

--- scripts/javaInstall.sh
+++ scripts/javaInstall.sh
@@ -3,8 +3,10 @@ java -version
 if [ $? == 0 ] && [[-n $JAVA_HOME]]; then
   echo "JAVA INSTALLED: " $JAVA_HOME
 else
-  sudo yum install -y java-1.8.0-openjdk 
-  sudo echo "export JAVA_HOME=/usr/lib/jvm/jre/bin" >> /etc/bashrc
-  export JAVA_HOME=/usr/lib/jvm/jre/bin
+  sudo yum install -y java-1.8.0-openjdk-devel 
+  sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.161-0.b14.el7_4.x86_64' >> /etc/profile
+  sudo echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile
+  sudo echo 'export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar' >> /etc/profile
+  source /etc/profile
 fi
 echo "JAVA INSTALLED: " $JAVA_HOME
